ce3df4b3c1b869ca687e2d96e551be6d291dc637
Jacob Hepkema <41913625+jacobhepkema@users.noreply.github.com>
2021-03-03 22:05:09 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+from setuptools import find_packages
+
+import sys
+
+def setup_package():
+  install_requires = ['numpy', 'pandas', 'tkinter', 'PIL', 'argparse']
+  metadata = dict(
+      name = 'meautifs',
+      version = '0.0.1',
+      description = 'annotpics: utilities for picture annotation',
+      url = 'https://github.com/jacobhepkema/annotpics',
+      author = 'Jacob Hepkema',
+      author_email = 'jacob.hepkema@sanger.ac.uk',
+      license = 'MIT License',
+      packages = find_packages(),
+      install_requires = install_requires
+    )
+
+  setup(**metadata)
+
+if __name__ == '__main__':
+  if sys.version_info < (2,7):
+    sys.exit('Sorry, Python < 2.7 is not supported')
+    
+  setup_package()
