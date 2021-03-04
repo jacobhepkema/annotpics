e48f76248dfc01ee923ac65cdaa101e13d3445b2
Jacob Hepkema <jacobhepkema@gmail.com>
2021-03-04 09:09:37 +0000

    more args

--- annotpics/__main__.py
+++ annotpics/__main__.py
@@ -1,20 +1,29 @@
 import sys
 import click
 
+from annotpics.interface import AnnotationInterface
+
 @click.version_option("0.0.1")
 @click.command()
 @click.argument('csv', required=True)
 @click.argument('image_dir', required=True)
 @click.argument('bindings', required=True)
 @click.option('--start_i', default=1, required=False)
-def main(csv, image_dir, bindings, start_i):
+@click.option('--window_w', default=764, required=False)
+@click.option('--window_h', default=1200, required=False)
+
+def main(csv, image_dir, bindings, start_i, 
+         window_w, window_h):
     r"""
-    Picture annotation tool
+    EXPERIMENTAL
+    Picture annotation tool.
     """
     print("csv =", csv)
     print("image_dir=", image_dir)
     print("bindings =", bindings)
     print("start_i =", start_i)
+    print("window_w =", window_w)
+    print("window_h =", window_h)
 
     pass
 
